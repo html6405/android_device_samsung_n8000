644bd588b2aa88dbdf0e57e17f9899f37cf00737
html6405 <peter.schelchshorn@zell-net.at>
2020-12-19 21:11:17 +0100

    Fix inherit row.

--- lineage_n8000.mk
+++ lineage_n8000.mk
@@ -2,9 +2,9 @@
 PRODUCT_RELEASE_NAME := n8000
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/n8000/n8000.mk)
-$(call inherit-product, device/samsung/n80xx-common/n80xx-common.mk)
 $(call inherit-product, device/samsung/smdk4412-common/common.mk)
+$(call inherit-product, device/samsung/n80xx-common/n80xx-common.mk)
+$(call inherit-product, device/samsung/n8000/n8000.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
