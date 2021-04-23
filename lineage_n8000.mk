04905f23937b4c8552f262ac2138b1181276944e
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-04-23 09:58:21 +0200

    Include TARGET_INIT_VENDOR_LIB and remove device fingerprint settings from lineage_nxxxx.mk.

--- lineage_n8000.mk
+++ lineage_n8000.mk
@@ -22,11 +22,3 @@ PRODUCT_NAME := lineage_n8000
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N8000
 PRODUCT_MANUFACTURER := samsung
-
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-		PRODUCT_NAME=p4noterfxx \
-		TARGET_DEVICE=p4noterf \
-		PRIVATE_BUILD_DESC="p4noterfxx-user 4.4.2 KOT49H N8000XXUDPB1 release-keys"
-
-BUILD_FINGERPRINT := samsung/p4noterfxx/p4noterf:4.4.2/KOT49H/N8000XXUDPB1:user/release-keys
