2800028d348ab943f6f2a7f03b849715720ee456
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-04-29 17:11:08 +0200

    Inherit n80xx-common.mk at first to use our own audio policy.

--- lineage_n8000.mk
+++ lineage_n8000.mk
@@ -2,8 +2,8 @@
 PRODUCT_RELEASE_NAME := n8000
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/smdk4412-common/common.mk)
 $(call inherit-product, device/samsung/n80xx-common/n80xx-common.mk)
+$(call inherit-product, device/samsung/smdk4412-common/common.mk)
 $(call inherit-product, device/samsung/n8000/n8000.mk)
 
 # Inherit from the common Open Source product configuration
